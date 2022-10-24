a934891c4be8b42e47674b00d15eb57bbdf477c2
Maniixer <maniixer@hotmail.com>
2022-10-24 14:02:20 +0200

    added wallet tabel section, sidebar radio buttons added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 import { walletData } from "./walletData.js";
+
+// Puts all the wallets their operating system arrays in one array.
+function getOperatingSystemArray(wallets) {
+  const operatingsystemArray = [];
+  for (let wallet of wallets) {
+    for (let operatingsystem of wallet.operatingSystem) {
+      operatingsystemArray.push(operatingsystem);
+    }
+  }
+  console.log(operatingsystemArray);
+}
+
+function renderOperatingSystemRadios(wallets) {
+  let radioItems = ``;
+  const operatingSystems = getOperatingSystemArray(wallets);
+  for (let operatingsystem of operatingSystems) {
+    radioItems += `<input type="radio" id="${operatingsystem}" value="${operatingsystem}" name="operatingSystems"`;
+  }
+}
+
+getOperatingSystemArray(walletData);
